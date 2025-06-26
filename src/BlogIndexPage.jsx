0086e7afb290cb027e39a10968ca336650f53165
Vishesh Chopra <61933415+KarmicKonquest@users.noreply.github.com>
2025-06-26 13:02:09 +0530

    Fix: Remove duplicate imports in BlogIndexPage

--- src/BlogIndexPage.jsx
+++ src/BlogIndexPage.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 function BlogIndexPage({ hubType }) {
